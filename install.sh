a70b25df196bf722f4904962b7781981f89413ff
Alfonso Aponte <alfonso@Alfonsos-MacBook-Pro.local>
2024-02-06 22:41:19 +0100

    adding install files

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 wget https://builds.matomo.org/matomo-latest.zip && \
-unzip matomo-latest.zip && \
+bsdtar -xvf matomo-latest.zip -s'|[^/]*/||' -C src/html && \
 rm matomo-latest.zip && \
+docker-compose up -d && \
 # DELETE IF ROW 10 WORKS: chown -R www-data:www-data /var/www/html
 
 
 echo -e "\033[0;33mSetting permissions\033[0m"
-docker exec --user=root -it  app chown www-data:www-data -R /var/www/html
+docker exec --user=root -it  matomo chown www-data:www-data -R /var/www/html
