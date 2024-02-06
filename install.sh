7b80b6d272a5572d1ad57783399694d7ad2251b8
Alfonso Aponte <alfonso@Alfonsos-MacBook-Pro.local>
2024-02-06 21:56:38 +0100

    creating separate docker files

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+wget https://builds.matomo.org/matomo-latest.zip && \
+unzip matomo-latest.zip && \
+rm matomo-latest.zip && \
+# DELETE IF ROW 10 WORKS: chown -R www-data:www-data /var/www/html
+
+
 echo -e "\033[0;33mSetting permissions\033[0m"
 docker exec --user=root -it  app chown www-data:www-data -R /var/www/html
