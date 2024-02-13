fda7862f3f065bbed25ba02c9f440931209f7712
Alfonso Aponte <alfonso@Alfonsos-MacBook-Pro.local>
2024-02-13 09:52:53 +0100

    working on CA

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ rm matomo-latest.zip && \
 docker-compose up -d && \
 # DELETE IF ROW 10 WORKS: chown -R www-data:www-data /var/www/html
 
+cd local-ssl &
+export CAROOT=$(pwd) &
+mkcert -install
 
 echo -e "\033[0;33mSetting permissions\033[0m"
 docker exec --user=root -it  matomo chown www-data:www-data -R /var/www/html
